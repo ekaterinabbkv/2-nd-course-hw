d64028cb231b91401879f97c6853eb3ad9f7a8ae
ekaterinabbkv <kir.ekaterina0793@mail.ru>
2023-07-10 01:34:54 +1000

    выполнены задания js

---
+++ index.js
@@ -0,0 +1,62 @@
+/*let a = 10;
+a = 20;
+alert (10);
+alert (20);*/
+
+/*let year = 2007
+alert (2007);*/
+
+/*let name = 'Brendan Eich';
+alert (name);*/
+
+/*let b = 10;
+let c = 2;
+alert (b + c);
+alert (b - c);
+alert (b * c);
+alert (b / c);*/
+
+/*let a = 2**5;
+let result = a;
+alert (result);*/
+
+/*let a = 9;
+let b = 2;
+alert (9 / 2);*/
+
+/*let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);*/
+
+/*let age;
+age = prompt ('Сколько вам лет?')
+alert (age);*/
+
+/*const user = {
+    name: 'Katya',
+    age: 30,
+    isAdmin: true
+}
+user.cityOfResidence = 'Якутск';
+user.age = 50;
+delete user.cityOfResidence;
+let info = prompt ('Какую информацию хотите узнать о пользователе');
+console.log(user[info]);
+alert (user[info]);*/
+
+/*let userName;
+userName = prompt ('Введите ваше имя');
+alert(`Привет, ${userName}!`);*/
+
+
+
+
+
+
+
+

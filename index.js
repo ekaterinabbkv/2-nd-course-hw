1931b68a8bee385dec5097e0ef58e1120f2f0d18
ekaterinabbkv <kir.ekaterina0793@mail.ru>
2023-07-12 02:43:07 +1000

    задания js

---
+++ index.js
@@ -0,0 +1,76 @@
+//Задание 1
+/*let password = 'пароль';
+let message = prompt ('Введите пароль');
+if (message === 'пароль') {
+    alert ('Пароль введен верно');
+} else {
+    alert ('Пароль введен не правильно');
+}*/
+
+//Задание 2
+/*let c = 10;
+if (c>=0 && c<=10) {
+    alert ('верно');
+} else {
+    alert ('Не верно')
+}*/
+
+//Задание 3
+/*let d = 2;
+let e = 200;
+if (d>100 || e>100) {
+    alert ('Верно');
+} else {
+    alert ('Не верно');    
+}*/
+
+//Задание 4
+/*let a = '2';
+let b = '3';
+// Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
+alert(2 + 3);*/
+
+//Задание 5
+/*let monthNumber = prompt ('Введите номер месяца');
+
+switch (monthNumber) {
+    case '1':
+        console.log ('Зима')
+        break;
+    case '2':
+        console.log ('Зима')
+        break;
+    case '12':
+        console.log ('Зима')
+        break;
+    case '3':
+        console.log ('Весна')
+        break;  
+    case '4':
+        console.log ('Весна')
+        break;
+    case '5':
+        console.log ('Весна')
+        break; 
+    case '6':
+        console.log ('Лето')
+        break;
+    case '7':
+        console.log ('Лето')
+        break;
+    case '8':
+        console.log ('Лето')
+        break;
+    case '9':
+        console.log ('Осень')
+        break;
+    case '10':
+        console.log ('Осень')
+        break;
+    case '11':
+        console.log ('Осень')
+        break;
+    default:
+        console.log ('Такого месяца не существует')
+        break;
+}*/

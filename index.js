fd4cd3eb7b8a4cbb214f66b4db7197c7749cbe81
ekaterinabbkv <kir.ekaterina0793@mail.ru>
2023-07-12 22:18:52 +1000

    задания js

--- index.js
+++ index.js
@@ -35,38 +35,22 @@ alert(2 + 3);*/
 
 switch (monthNumber) {
     case '1':
-        console.log ('Зима')
-        break;
     case '2':
-        console.log ('Зима')
-        break;
     case '12':
         console.log ('Зима')
         break;
     case '3':
-        console.log ('Весна')
-        break;  
     case '4':
-        console.log ('Весна')
-        break;
     case '5':
         console.log ('Весна')
-        break; 
+        break;  
     case '6':
-        console.log ('Лето')
-        break;
     case '7':
-        console.log ('Лето')
-        break;
     case '8':
         console.log ('Лето')
         break;
     case '9':
-        console.log ('Осень')
-        break;
     case '10':
-        console.log ('Осень')
-        break;
     case '11':
         console.log ('Осень')
         break;

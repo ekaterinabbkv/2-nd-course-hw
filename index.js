f20a089cf1d547ba3b6214bd791d514aa12dbb94
ekaterinabbkv <kir.ekaterina0793@mail.ru>
2023-07-14 03:21:29 +1000

    js

--- index.js
+++ index.js
@@ -1,72 +1,78 @@
 //Задание 1
-/*let a = 10;
-alert (10);
-a = 20;
-alert (20);
+/*let password = 'пароль';
+let message = prompt ('Введите пароль');
+if (message === password) {
+    alert ('Пароль введен верно');
+} else {
+    alert ('Пароль введен не правильно');
+}*/
 
 //Задание 2
-/*let year = 2007
-alert (year);
+/*let c = 10;
+if (c>=0 && c<=10) {
+    console.log ('верно');
+} else {
+    console.log ('Не верно')
+}*/
 
 //Задание 3
-let name = 'Brendan Eich';
-alert (name);
+/*let d = 2;
+let e = 200;
+if (d>100 || e>100) {
+    console.log ('Верно');
+} else {
+    console.log ('Не верно');    
+}*/
 
 //Задание 4
-let b = 10;
-let c = 2;
-alert (b + c);
-alert (b - c);
-alert (b * c);
-alert (b % c);
+/*let a = '2';
+let b = '3';
+// Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
+a = Number(a);
+b = Number (b);
+alert(a + b);*/
 
 //Задание 5
-let a = 2**5;
-let result = a;
-alert (result);
+/*let monthNumber = prompt ('Введите номер месяца');
+
+switch (monthNumber) {
+    case '1':
+    case '2':
+    case '12':
+        console.log ('Зима')
+        break;
+    case '3':
+    case '4':
+    case '5':
+        console.log ('Весна')
+        break;  
+    case '6':
+    case '7':
+    case '8':
+        console.log ('Лето')
+        break;
+    case '9':
+    case '10':
+    case '11':
+        console.log ('Осень')
+        break;
+    default:
+        console.log ('Такого месяца не существует')
+        break;
+}*/
 
 //Задание 6
-let a = 9;
-let b = 2;
-alert (9 / 2);
+/*адаптивный сайт*/
 
 //Задание 7
-let num = 1;
-num += 5;
-num -= 3;
-num *= 7;
-num /= 3;
-num ++;
-num --;
-alert(num);
+/*let number = prompt ('Пожалуйста, введите любое число');
+console.log (isNaN(number));
 
-//Задание 8
-let age;
-age = prompt ('Сколько вам лет?')
-alert (age);
-
-//Задание 9
-const user = {
-    name: 'Katya',
-    age: 30,
-    isAdmin: true
-}
-
-//Задание 9.1
-user["city of residence"] = 'Якутск';
-
-//Задание 9.2
-user.age = 50;
-
-//Задание 9.3
-delete user["city of residence"];
-
-//Задание 9.4
-let info = prompt ('Какую информацию хотите узнать о пользователе');
-console.log(user[info]);
-alert (user[info]);
-
-//Задание 10
-let userName;
-userName = prompt ('Введите ваше имя');
-alert(`Привет, ${userName}!`);*/
+if (isNaN(number)) {
+    alert('Не является числом');
+  }
+  else if (number % 2) {
+      alert ('Число не четное');
+  } else {
+      alert ('Число четное');
+  }*/

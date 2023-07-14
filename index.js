9718261dc426087d8d71fdc5cb6301b96ca094d0
ekaterinabbkv <kir.ekaterina0793@mail.ru>
2023-07-15 01:40:58 +1000

    js задания

--- index.js
+++ index.js
@@ -6,3 +6,46 @@ while (i < 2) {
     i++;
 }*/
 
+//Задание 2
+
+/*let i = 1;
+while (i <= 5) {
+    console.log (i);
+    i++;
+}*/
+
+//Задание 3
+/*let i = 7;
+while (i <= 22) {
+    console.log (i);
+    i++;
+}*/
+
+//Задание 4
+
+/*const obj = {
+    'Коля' : 200,
+    'Вася' : 300,
+    'Петя' : 400
+};
+
+for (key in obj) {
+        console.log ([key]+ '- зарплата ' + obj[key] + ' долларов.');
+    
+}*/
+
+//Задание 5
+
+/*let n = 1000;
+let num = 0;
+while (n >= 50) {
+    n /= 2;
+    num++;
+    }
+console.log (n);
+console.log (num);*/
+
+//Задание 6
+
+/*let a = 5;
+let dayNumber = 31;

c77b31f67e4dd4bfa7dd63f3f9c1db7e0a9250f8
ekaterinabbkv <kir.ekaterina0793@mail.ru>
2023-07-15 00:09:35 +1000

    js задания

--- index.js
+++ index.js
@@ -1,78 +1,8 @@
 //Задание 1
-/*let password = 'пароль';
-let message = prompt ('Введите пароль');
-if (message === password) {
-    alert ('Пароль введен верно');
-} else {
-    alert ('Пароль введен не правильно');
+
+/*let i = 0;
+while (i < 2) {
+    console.log ('Привет');
+    i++;
 }*/
 
-//Задание 2
-/*let c = 10;
-if (c>=0 && c<=10) {
-    console.log ('верно');
-} else {
-    console.log ('Не верно')
-}*/
-
-//Задание 3
-/*let d = 2;
-let e = 200;
-if (d>100 || e>100) {
-    console.log ('Верно');
-} else {
-    console.log ('Не верно');    
-}*/
-
-//Задание 4
-/*let a = '2';
-let b = '3';
-// Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
-a = Number(a);
-b = Number (b);
-alert(a + b);*/
-
-//Задание 5
-/*let monthNumber = prompt ('Введите номер месяца');
-
-switch (monthNumber) {
-    case '1':
-    case '2':
-    case '12':
-        console.log ('Зима')
-        break;
-    case '3':
-    case '4':
-    case '5':
-        console.log ('Весна')
-        break;  
-    case '6':
-    case '7':
-    case '8':
-        console.log ('Лето')
-        break;
-    case '9':
-    case '10':
-    case '11':
-        console.log ('Осень')
-        break;
-    default:
-        console.log ('Такого месяца не существует')
-        break;
-}*/
-
-//Задание 6
-/*адаптивный сайт*/
-
-//Задание 7
-/*let number = prompt ('Пожалуйста, введите любое число');
-console.log (isNaN(number));
-
-if (isNaN(number)) {
-    alert('Не является числом');
-  }
-  else if (number % 2) {
-      alert ('Число не четное');
-  } else {
-      alert ('Число четное');
-  }*/

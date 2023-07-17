09e3a1d9e9cb9a067b007b33e32673f052523b7a
ekaterinabbkv <kir.ekaterina0793@mail.ru>
2023-07-18 02:35:08 +1000

    решение задач

---
+++ index.js
@@ -0,0 +1,65 @@
+//Задание 1
+
+/*function min(a , b) {
+    if (a < b) {
+      return a;  
+    } else {
+        return b;
+    }
+}
+
+console.log(min(8, 4));*/
+
+//Задание 2
+
+/*function num(c) {
+    if (c % 2 == 0) {
+      return ('Число четное');  
+    } else {
+        return ('Число нечетное');
+    }
+}
+
+console.log(num(9));*/
+
+//Задание 3
+//3.1
+
+/*function num(i) {
+    i**2;
+}
+
+console.log(num(10));*/
+
+//3.2
+
+/*function num(i) {
+    return (i ** 2);
+}
+console.log(num(10));*/
+
+//Задание 4
+
+/*let age = prompt ('Сколько вам лет?');
+
+if (age <= 12) {
+    function printMessage() {
+        console.log('Привет, друг!');
+    }
+} else {
+    function printMessage() {
+        console.log('Добро пожаловать!');
+    }
+}
+if (age < 0) {
+    function printMessage() {
+        console.log('Вы ввели неправильное значение');
+    }
+}
+printMessage();*/
+
+//Задание 5
+
+function num(a , b) {
+    
+}

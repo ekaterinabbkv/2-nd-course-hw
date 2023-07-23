e35b6829ef2f32d7adbb3a7189050cecd4b7400d
ekaterinabbkv <kir.ekaterina0793@mail.ru>
2023-07-24 03:22:29 +1000

    задание js

--- index.js
+++ index.js
@@ -121,17 +121,17 @@ filterPositive(num);*/
 function seasons() {
     let answer = prompt('Введите номер месяца (от 1 до 12)');
     
-    if (answer = 12 || (answer >= 1 && answer <=2)) {
+    if (answer == 12 || answer == 1 || answer == 2) {
         alert('Зима');
     }
-    else if (answer >= 3 && answer <= 5) {
+    else if (answer == 3 || answer == 4 || answer == 5) {
         alert('Весна');
     }
-    else if (answer >= 6 && answer <= 8) {
+    else if (answer == 6 || answer == 7 || answer == 8) {
         alert('Лето');
     }
-    else if (answer >= 9 && answer <= 11) {
-        alert('Осень');;
+    else if (answer == 9 || answer == 10 || answer == 11) {
+        alert('Осень');
     }
     else {
         alert('Вы ввели не подходящее значение');
